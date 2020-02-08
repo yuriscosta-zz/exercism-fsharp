7216819ba92f6f0720fdf9cb3f3fd09e41499669
yuri.algoritmus <yuri@algoritmus.com.br>
2020-02-08 20:22:02 -0300

    Add some challenges

---
+++ myFSharpApp/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+// Define a new function to print a name.
+// It is defined above the main function
+let printGreeting name =
+    printfn "Hello %s from F#!" name
+
+[<EntryPoint>]
+let main argv =
+    printGreeting "Yuri"
+    0 // return an integer exit code
---
+++ two-fer/TwoFer.fs
@@ -0,0 +1,6 @@
+﻿module TwoFer
+
+let twoFer (input: string option): string =
+    match input with
+    | Some(input) -> "One for " + input + ", one for me."
+    | None -> "One for you, one for me."
