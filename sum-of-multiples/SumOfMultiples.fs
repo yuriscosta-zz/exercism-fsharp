978844f8de0ef4a3fa7cad8dca9a84a58532ed5f
yuriscosta <yuri.sales@protonmail.com>
2020-02-09 15:30:29 -0300

    Add more challeges solutions and improvements

---
+++ sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,8 @@
+﻿module SumOfMultiples
+
+let sum (numbers: int list) (upperBound: int): int =
+    let isMultiple x = List.exists (fun y -> y <> 0 && x % y = 0) numbers
+
+    [ 1 .. upperBound - 1 ]
+    |> List.filter isMultiple
+    |> List.sum
--- two-fer/TwoFer.fs
+++ two-fer/TwoFer.fs
@@ -1,6 +1,6 @@
 ﻿module TwoFer
 
 let twoFer (input: string option): string =
-    match input with
-    | Some(input) -> "One for " + input + ", one for me."
-    | None -> "One for you, one for me."
+    input
+    |> Option.defaultValue "you"
+    |> sprintf "One for %s, one for me."
